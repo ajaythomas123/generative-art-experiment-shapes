1c48911e9f59c2f2eb388223becc27db02af5261
Ajay Thomas <ajaythomas123@gmail.com>
2021-11-29 21:00:06 +0530

    Add Arc class

--- src/main.js
+++ src/main.js
@@ -43,26 +43,30 @@ function generateArc(ctx, radius, center, position, color = '#000000') {
   ctx.beginPath();
   let startAngle;
   let endAngle;
+  let corners = generateTriangleCorners(radius, center, position);
   switch (position) {
     case DIRECTIONS.TR:
-      startAngle = 1.5 * Math.PI;
+      startAngle = 0.5 * Math.PI;
       endAngle = Math.PI;
       break;
     case DIRECTIONS.BL:
-      startAngle = 0.5 * Math.PI;
-      endAngle = 0;
+      startAngle = 1.5 * Math.PI;
+      endAngle = 2 * Math.PI;
       break;
     case DIRECTIONS.BR:
       startAngle = Math.PI;
-      endAngle = 0.5 * Math.PI;
+      endAngle = 1.5 * Math.PI;
       break;
     default:
     case DIRECTIONS.TL:
       startAngle = 0;
-      endAngle = 1.5 * Math.PI;
+      endAngle = 0.5 * Math.PI;
       break;
   }
-  ctx.arc(center.x, center.y, radius, startAngle, endAngle);
+  ctx.arc(corners[0].x, corners[0].y, radius, startAngle, endAngle);
+  ctx.moveTo(corners[1].x, corners[1].y);
+  ctx.lineTo(corners[0].x, corners[0].y);
+  ctx.lineTo(corners[2].x, corners[2].y);
   ctx.strokeStyle = color;
   ctx.fillStyle = color;
   ctx.stroke();
@@ -86,6 +90,11 @@ function generateTriangle(ctx, corners, color = "#000000") {
 
 function generateTriangleCorners(side, tlCorner, rightAngleCornerPosition) {
   const { x, y } = tlCorner;
+  /**
+   * corner[0] = Right angle corner
+   * corner[1] = Corner vertically distant from right angle corner.
+   * corner[2] = Corner horizontally distant from right angle corner.
+   */
   switch (rightAngleCornerPosition) {
     case DIRECTIONS.TR:
       return [{ x: x + side, y }, { x: x + side, y: y + side }, { x, y }];
@@ -138,7 +147,6 @@ function getAdjacentCoordinate(distance, coordinates, direction) {
 }
 
 function getSquareCenter(side, tlCorner) {
-  console.log(tlCorner, side)
   return {
     x: tlCorner.x + side / 2,
     y: tlCorner.y + side / 2,
@@ -150,67 +158,36 @@ function getSquareCenter(side, tlCorner) {
 /** START - Shape classes */
 
 class Circle {
-  constructor(ctx, work, diameter, corner) {
-    const previousWork = work || [];
+  constructor(ctx, diameter, corner) {
     this.ctx = ctx;
     this.radius = diameter / 2;
     this.corner = corner;
-    this.work = [...previousWork, this];
   }
-
-  getTop = () => {
-    const newCorner = getAdjacentCoordinate(
-      2 * this.radius,
-      this.corner,
-      DIRECTIONS.T
-    );
-    return new Circle(this.ctx, this.work, this.radius, newCorner);
-  };
-
-  getRight = () => {
-    const newCorner = getAdjacentCoordinate(
-      2 * this.radius,
-      this.corner,
-      DIRECTIONS.R
-    );
-    return new Circle(this.ctx, this.work, this.radius, newCorner);
-  };
-
-  getBottom = () => {
-    const newCorner = getAdjacentCoordinate(
-      2 * this.radius,
-      this.corner,
-      DIRECTIONS.B
-    );
-    return new Circle(this.ctx, this.work, this.radius, newCorner);
-  };
-
-  getLeft = () => {
-    const newCorner = getAdjacentCoordinate(
-      2 * this.radius,
-      this.corner,
-      DIRECTIONS.L
-    );
-    return new Circle(this.ctx, this.work, this.radius, newCorner);
-  };
 
   render = (draw) => {
     const center = getSquareCenter(2 * this.radius, this.corner);
-    if (draw) {
-      generateCircle(this.ctx, this.radius, center, generateRandomColor());
-    } else {
-      this.work.forEach((circle) => circle.render(true));
-    }
+    generateCircle(this.ctx, this.radius, center, generateRandomColor());
   };
 }
 
+class Arc {
+  constructor(ctx, side, corner) {
+    this.ctx = ctx;
+    this.radius = side;
+    this.corner = corner;
+  }
+
+  render(position) {
+    const color = generateRandomColor();
+    generateArc(this.ctx, this.radius, this.corner, position, color);
+  }
+}
+
 class Triangle {
-  constructor(ctx, work, side, corner) {
-    const previousWork = work || [];
+  constructor(ctx, side, corner) {
     this.ctx = ctx;
     this.side = side;
     this.corner = corner;
-    this.work = [...previousWork, this];
   }
 
   render(position) {
@@ -228,13 +205,13 @@ const SIDE = 40;
 let startCorner = { x: 0, y: 0 };
 let corner = startCorner;
 
-const SHAPES = [Circle, Triangle];
+const SHAPES = [Circle, Arc, Triangle];
 const renderLoop = () => {
   for (let i = 0; i < 11; i++) {
-    const shapeRandomNumber = randomIntFromInterval(0, 1);
+    const shapeRandomNumber = randomIntFromInterval(0, 2);
     const shape = SHAPES[shapeRandomNumber];
     let position = generateRandomCornerPosition();
-    const instance = new shape(ctx, [], SIDE, corner);
+    const instance = new shape(ctx, SIDE, corner);
     instance.render(position);
     corner = getAdjacentCoordinate(SIDE, corner, DIRECTIONS.R);
   }
