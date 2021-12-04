8eab43233bc4bdb1c1e8b8ccd2ea02c7bead8787
Ajay Thomas <ajaythomas123@gmail.com>
2021-12-04 05:32:03 +0530

    Add brotli and gzip compression in webpack config

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -6,6 +6,9 @@ const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").def
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require('zlib');
+
 module.exports = merge(CommonConfig, {
   output: {
     path: path.join(__dirname, '../dist'),
@@ -22,6 +25,26 @@ module.exports = merge(CommonConfig, {
     },
   },
   plugins: [
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: false,
+    }),
     new MiniCssExtractPlugin({ filename: 'styles-[contenthash].css' }),
     new HTMLInlineCSSWebpackPlugin(),
     new HtmlInlineScriptPlugin(),
